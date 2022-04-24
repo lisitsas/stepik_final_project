a1620bd7f8312a404b77db1c96ed413d3427542d
Aleksandr Lisitca <al@woodenshark.com>
2022-04-24 20:30:04 +0300

    Add class LoginPage

---
+++ pages/login_page.py
@@ -0,0 +1,21 @@
+from .base_page import BasePage
+from selenium import webdriver
+from .locators import LoginPageLocators
+
+
+class LoginPage(BasePage):
+    def should_be_login_page(self):
+        self.should_be_login_url()
+        self.should_be_login_form()
+        self.should_be_register_form()
+
+    def should_be_login_url(self):
+        current_link = webdriver.current_url
+        index = current_link.find("login")
+        assert index != -1, f"Expected 'login' is substring of '{current_link}'"
+
+    def should_be_login_form(self):
+        assert self.browser.find_element(*LoginPageLocators.LOGIN_FORM), "No login form"
+
+    def should_be_register_form(self):
+        assert self.browser.find_element(*LoginPageLocators.REGISTER_FORM), "No register form"
